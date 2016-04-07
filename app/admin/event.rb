8aa11447fa35a2f172b373971f69f9bdded6d97b
Aman Bhal <amandeep.bhal92@tamu.edu>
2016-04-07 03:35:36 +0000

    Improved Dashboard

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,17 +1,17 @@
 ActiveAdmin.register Event do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title, :description, :venue, :date, :start_time, :end_time, :ticket_price
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
-
+    permit_params :title, :description, :venue, :date, :start_time, :end_time, :ticket_price
+    
+    scope :past_events
+    scope :future_events
+    scope :ongoing_events
+    index do
+        column :title
+        column :date
+        column :venue
+        column :start_time
+        column :end_time
+        column :ticket_price
+        actions
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
     def index
         @events = Event.order('id desc').all
     end
+    
+    scope :past_events,->{ where('date < ?', Date.today) }
+    scope :future_events,->{ where('date > ?', Date.today) }
+    scope :ongoing_events,->{ where('date == ?', Date.today) }
 end
